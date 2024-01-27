48335986575dabd7e4b02c0cd67108a5af10dae4
Titouan Launay <titouan.launay@lleedpartners.com>
2024-01-27 12:07:42 -0800

    Adds basic js client

---
+++ packages/client-js/src/client.ts
@@ -0,0 +1,35 @@
+export interface DocumentInput {
+  html: string;
+  metadata?: {
+    title?: string;
+    author?: string;
+    subject?: string;
+    keywords?: string[];
+    date?: string;
+  };
+  assets?: (
+    | {
+        path: string;
+        content: Buffer;
+      }
+    | { href: string }
+  )[];
+}
+
+export class Onedoc {
+  private apiKey: string;
+  private endpoint: string = "http://localhost:3000";
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+
+  private buildUrl(path: string) {
+    return `${this.endpoint}${path}`;
+  }
+
+  async render(document: DocumentInput) {
+    // Fetch the /api/docs/initiate API endpoint
+    const information = await fetch(this.buildUrl("/docs/initiate"), {});
+  }
+}
---
+++ packages/client-js/src/index.ts
@@ -0,0 +1,1 @@
+export { Onedoc } from "./client";
