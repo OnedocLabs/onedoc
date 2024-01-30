1583c91775bd732ff3bd94b0a9492f276bd0df33
Titouan Launay <titouan.launay@lleedpartners.com>
2024-01-30 08:58:10 -0800

    Refactor HtmlBuilder class

--- packages/client-js/src/htmlBuilder.ts
+++ packages/client-js/src/htmlBuilder.ts
@@ -1,38 +1,32 @@
-
-
 export class HtmlBuilder {
-
-    private title: string = "Onedoc"
-    private start: string = `<!DOCTYPE html>
+  private title: string = "Onedoc";
+  private start: string = `<!DOCTYPE html>
                              <html  lang="en">
                                 <head>
                                     <meta charset = "UTF-8">
                                     <meta name="viewport" content="width=device-width">`;
-    private middle: string = `<title>Onedoc</title>
+  private middle: string = `<title>Onedoc</title>
                                 </head>
-                                <body>`
+                                <body>`;
 
-    private end: string =`</body>
-                        </html>`
+  private end: string = `</body>
+                        </html>`;
 
-    private styleSheets:string[];
-  
-    constructor(title: string) {
-        this.title = title
+  private styleSheets: string[];
+
+  constructor(title: string) {
+    this.title = title;
+  }
+
+  build(react: string, styleSheets?: string[]) {
+    if (styleSheets) {
+      styleSheets.forEach((path) => {
+        this.start += `<link rel = "stylesheet" href=${path} />`;
+      });
     }
 
-    build(react:string, styleSheets?:string[]){
+    this.middle += react;
 
-        if (styleSheets){
-            
-            styleSheets.forEach((path) =>{
-                this.start+= `<link rel = "stylesheet" href=${path} />`
-            })
-        }
-
-        this.middle += react;
-
-        return this.start+this.middle+this.end;
-    }
-
+    return this.start + this.middle + this.end;
+  }
 }
