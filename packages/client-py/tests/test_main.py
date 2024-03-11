f7d11ba1ebf8e16a00fff232d0b3b588f47b4028
Pierre Dorge <pierre.dorge@onedoclabs.com>
2024-03-11 08:50:54 -0700

    Adds comments in test_main

--- packages/client-py/tests/test_main.py
+++ packages/client-py/tests/test_main.py
@@ -12,4 +12,22 @@ document = {
 # Render the document
 result = onedoc.render(document)
 
+#for files that are not saved, remember to use "wb" when writing file
+# onedoc = Onedoc(api_key)
+
+# # Define your document
+# document = {
+#     "html": "<h1>Hello World</h1>",  # Simple HTML content
+#     "title": "My First Document",
+#     "test": True,  # Set to False to use in production
+#     "save": False,  # Set to True if you want to save the document
+# }
+
+# # Render the document
+# result = onedoc.render(document)
+# print(result.get("file"))
+# f = open("hellowordpierre.pdf", "wb")
+# f.write(result.get("file"))
+# f.close()
+
 print(result)
