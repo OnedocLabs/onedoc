6a2d945d24dc53499376dadc4b4c1054ae013c8e
Pierre Dorge <pierre.dorge@onedoclabs.com>
2024-03-11 19:46:28 -0700

    Adds watermark and stylesheets support to python sdk

--- packages/client-py/onedoc/test.py
+++ packages/client-py/onedoc/test.py
@@ -6,17 +6,30 @@ load_dotenv()
 
 onedoc = Onedoc(getenv("ONEDOC_API_KEY"))
 
+
+with open("packages/client-py/onedoc/styles.css", 'r') as file:
+    # Read the entire content of the file into a variable
+    css_content = file.read()
+print(css_content)
+
 # Define your document
 document = {
     "html": "<h1>Table of contents</h1><a href='file://test.pdf#page=1'>First page</a><br/><a href='file://test.pdf#page=2'>Second page</a>",  # Simple HTML content
     "title": "My First Document",
-    "test": True,  # Set to False to use in production
-    "save": False,  # Set to True if you want to save the document
+    "test": False,  # Set to False to use in production
+    "save": True,  # Set to True if you want to save the document
+    "assets":[
+        {
+            "content": css_content ,
+            "path": "/styles.css"
+        }
+    ]
 }
 
 # Render the document
-#result = onedoc.render(document)
+result = onedoc.render(document)
 
+print(result)
 #for files that are not saved, remember to use "wb" when writing file
 # onedoc = Onedoc(api_key)
 
@@ -40,12 +53,12 @@ document = {
 #f.write(result.get("file"))
 #f.close()
 
-firstFile = open("toc.pdf", "rb")
-secondFile = open("test.pdf", "rb")
+# firstFile = open("toc.pdf", "rb")
+# secondFile = open("test.pdf", "rb")
 
-result = onedoc.merge(firstFile, "toc.pdf", secondFile, "test.pdf")
+# result = onedoc.merge(firstFile, "toc.pdf", secondFile, "test.pdf")
 
-# Result is a pdf file
-f = open("merged.pdf", "wb")
-f.write(result.get('file'))
-f.close()
+# # Result is a pdf file
+# f = open("merged.pdf", "wb")
+# f.write(result.get('file'))
+# f.close()
